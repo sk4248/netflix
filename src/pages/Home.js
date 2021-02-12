ec476f392d916d75b1ca52ea8711cd4721d86f0c
sreekanth korampally <srikanth6006@gmail.com>
2021-02-12 00:06:24 -0600

    adds styling fot the footer component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { JumbotronContainer } from "containers/jumbotron";
+import { FooterContainer } from "containers/footer";
 
 const Home = () => {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 };
 
 export default Home;
